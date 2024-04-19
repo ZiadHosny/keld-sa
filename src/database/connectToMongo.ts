c225184868bf51f09408173b42d6d51da8cb6b42
ziad <ziad.hosny@bitspire.ca>
2024-04-19 23:21:01 +0200

    connect To DB

---
+++ src/database/connectToMongo.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { getFromEnv } from "../utils/getFromEnv.js";
+import { logErrMsg, logErrInfoMsg, logSuccessMsg } from "../utils/console/log.js";
+
+export const connectToMongoDb = async () => {
+
+    const { mongoDBUrl } = getFromEnv()
+
+    try {
+        await mongoose.connect(mongoDBUrl)
+        logSuccessMsg(`Connect To Mongo DB Successfully`)
+    } catch (err) {
+        logErrMsg('Error when connect to Mongo DB')
+        logErrInfoMsg(err)
+    }
+}
